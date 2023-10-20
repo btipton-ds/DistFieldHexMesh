1233fb8b79f9effe59a603215f6bbf93c1db88a0
btipton-ds <btipton@darkskyinnovation.com>
2023-10-20 14:37:13 +0800

    Improved thread safety on the object pool

--- DistFieldHexMesh/include/objectPool.h
+++ DistFieldHexMesh/include/objectPool.h
@@ -6,6 +6,80 @@
 
 namespace DFHM {
 
+template<class T, int BITS>
+class SegmentedVector {
+public:
+	T& operator[](size_t idx);
+	const T& operator[](size_t idx) const;
+	void push_back(const T& val);
+	size_t size() const;
+	void reserve(size_t size);
+	void clear();
+
+	std::recursive_mutex& getMutex() const;
+private:
+	mutable std::recursive_mutex _mutex;
+
+	std::vector<std::shared_ptr<std::vector<T>>> _data;
+};
+
+template<class T, int BITS>
+T& SegmentedVector<T, BITS>::operator[](size_t idx)
+{
+	const size_t mask = (((size_t)1) << BITS) - 1;
+	std::lock_guard<std::recursive_mutex> lock(_mutex);
+	size_t idx0 = idx >> BITS;
+	size_t idx1 = idx & mask;
+	return _data[idx0]->operator[](idx1);
+}
+
+template<class T, int BITS>
+const T& SegmentedVector<T, BITS>::operator[](size_t idx) const
+{
+	const size_t mask = (((size_t)1) << BITS) - 1;
+	std::lock_guard<std::recursive_mutex> lock(_mutex);
+	size_t idx0 = idx >> BITS;
+	size_t idx1 = idx & mask;
+	return _data[idx0]->operator[](idx1);
+}
+
+template<class T, int BITS>
+void SegmentedVector<T, BITS>::push_back(const T& val)
+{
+	const size_t maxSize = (((size_t)1) << BITS);
+	std::lock_guard<std::recursive_mutex> lock(_mutex);
+	if (_data.empty() || _data.back()->size() >= maxSize) {
+		auto pNew = std::make_shared <std::vector<T>>();
+		pNew->reserve(maxSize);
+		pNew->push_back(val);
+		_data.push_back(pNew);
+	} else {
+		_data.back()->push_back(val);
+	}
+}
+
+template<class T, int BITS>
+size_t SegmentedVector<T, BITS>::size() const
+{
+	std::lock_guard<std::recursive_mutex> lock(_mutex);
+	size_t result = 0;
+	for (auto p : _data)
+		result += p->size();
+	return result;
+}
+
+template<class T, int BITS>
+inline std::recursive_mutex& SegmentedVector<T, BITS>::getMutex() const
+{
+	return _mutex;
+}
+
+template<class T, int BITS>
+void SegmentedVector<T, BITS>::clear()
+{
+	_data.clear();
+}
+
 template<class T>
 class ObjectPool {
 public:
@@ -29,25 +103,23 @@ public:
 private:
 	ObjectPool(const ObjectPool& src) = default;
 
-	std::mutex _mutex;
 	std::vector<size_t> _idToIndexMap;
 	std::vector<size_t> _availableData;
 	std::vector<size_t> _availableIds;
-	std::vector<T> _pool;
+	SegmentedVector<T, 8> _pool;
 };
 
 template<class T>
 ObjectPool<T>::ObjectPool()
 {
-	std::lock_guard<std::mutex> lock(_mutex);
-	_pool.reserve(1000);
+	std::lock_guard<std::recursive_mutex> lock(_pool.getMutex());
 	_idToIndexMap.reserve(1000);
 }
 
 template<class T>
 void ObjectPool<T>::testReset()
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	std::lock_guard<std::recursive_mutex> lock(_pool.getMutex());
 	_idToIndexMap.clear();
 	_availableData.clear();
 	_availableIds.clear();
@@ -59,7 +131,7 @@ void ObjectPool<T>::free(size_t id)
 {
 	if (id > _idToIndexMap.size())
 		return;
-	std::lock_guard<std::mutex> lock(_mutex);
+	std::lock_guard<std::recursive_mutex> lock(_pool.getMutex());
 	size_t index = _idToIndexMap[id];
 	if (index != -1) {
 		_availableData.push_back(index);
@@ -71,7 +143,7 @@ void ObjectPool<T>::free(size_t id)
 template<class T>
 void ObjectPool<T>::unload(size_t id)
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	std::lock_guard<std::recursive_mutex> lock(_pool.getMutex());
 	size_t index = _idToIndexMap[id];
 	_pool[index] = {};
 	if (index != -1) 
@@ -89,7 +161,7 @@ inline size_t ObjectPool<T>::create(size_t id /* = -1*/)
 template<class T>
 size_t ObjectPool<T>::create(T*& pObj, size_t id /* = -1*/)
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	std::lock_guard<std::recursive_mutex> lock(_pool.getMutex());
 
 	size_t index = -1;
 	if (id >= _idToIndexMap.size()) {
@@ -130,7 +202,7 @@ size_t ObjectPool<T>::getObj(size_t id, T*& pObj, bool allocateIfNeeded)
 		result = create();
 	
 	{
-		std::lock_guard<std::mutex> lock(_mutex);
+		std::lock_guard<std::recursive_mutex> lock(_pool.getMutex());
 		pObj = &_pool[_idToIndexMap[result]];
 	}
 	return result;
@@ -139,7 +211,7 @@ size_t ObjectPool<T>::getObj(size_t id, T*& pObj, bool allocateIfNeeded)
 template<class T>
 const T* ObjectPool<T>::getObj(size_t id) const
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	std::lock_guard<std::recursive_mutex> lock(_pool.getMutex());
 	if (id != -1 && id < _idToIndexMap.size()) {
 		return &_pool[_idToIndexMap[id]];
 	}
@@ -149,7 +221,7 @@ const T* ObjectPool<T>::getObj(size_t id) const
 template<class T>
 T* ObjectPool<T>::getObj(size_t id)
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	std::lock_guard<std::recursive_mutex> lock(_pool.getMutex());
 	if (id != -1 && id < _idToIndexMap.size()) {
 		return &_pool[_idToIndexMap[id]];
 	}
